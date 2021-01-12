109a357eb7217c01c10e449432fb767c79e168a6
Daniyal_Akram_07 <77106224+daniyal-akram@users.noreply.github.com>
2021-01-12 12:05:55 +0500

    Add files via upload

---
+++ temperature.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float celsius,Fernhit;
+	cout<<"entre the Temperature :";
+	cin>>celsius;
+	Fernhit=32+(celsius*80/100);
+	cout<<"temperature in Fernhit :"<<Fernhit;
+	return 0;
+ } 
